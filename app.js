fb13461fed5b1e2d2871105fe360235bd086f9e2
Barry-owino <barryowino5@gmail.com>
2022-07-07 08:35:29 +0300

    added the auto type text on the hero div

---
+++ app.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+var typed = new Typed(".auto-input", {
+  strings: ["BARRY OWINO", "WEB DEVELOPER", "FRONTEND ENGINEER"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+})
